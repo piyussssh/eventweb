e2d9c7a1dab9af3da949f14838a2809df1028ce9
piyussssh <mvps1862@gmail.com>
2023-03-12 00:37:24 +0530

    cards & card page created

---
+++ src/components/card.js
@@ -0,0 +1,30 @@
+import {MdMoreVert} from 'react-icons/md'
+const Card = () => {
+  return (
+    <>
+      <div className="c-card p-4 card border">
+        <img
+          className="card-img"
+          src={require("../img/imgone.jpg")}
+          alt="img"
+        />
+        <div className="card-content pt-3">
+            <div className="d-flex justify-content-between">
+          <h2 className="c-s-2 c-head">Event Heading One</h2>
+          <span className=''><MdMoreVert className='c-s-2'/></span>
+
+            </div>
+          <p className="c-s-4 c-body">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+          <div className=" d-flex justify-content-between">
+            <span className="c-s-3 ">21k Interested</span>
+            <span className="c-s-3">2k Booked</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
+import Navbarc from "../components/Navbar";
+import Card from "../components/card"
+
 const Home = () => {
     return(
         <>
-        <div className="container">
+        <Navbarc/>
+        <div className="container mt-4 flex-wrap d-flex gap-5 justify-content-center justify-content-lg-start">
+           
+            
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
             
 
         </div>
